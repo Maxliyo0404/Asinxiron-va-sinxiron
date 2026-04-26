9ea88c54f9f6249a8bece139f595e92b4191e620
Mahliyo <gasandigital13@gmail.com>
2026-04-26 18:30:31 +0500

    fetch javascript [

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 let wrapper = document.getElementById("cards");
 fetch("https://699d9b4283e60a406a46e1ba.mockapi.io/Teachers")
-then.(respone =>respone.json())
-then(data =>
+.then(respone =>respone.json())
+.then(data => {
     console.log(data);
-    
-)
+})
