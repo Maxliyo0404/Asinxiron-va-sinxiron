a60be15ad6253053711c974ea170e5d647a24cea
Mahliyo <gasandigital13@gmail.com>
2026-04-26 18:28:03 +0500

    fetch

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 let wrapper = document.getElementById("cards");
 fetch("https://699d9b4283e60a406a46e1ba.mockapi.io/Teachers")
 then.(respone =>respone.json())
+then(data =>
+    console.log(data);
+    
+)
