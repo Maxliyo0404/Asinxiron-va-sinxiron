39c5470f69c8be0e5e76b03e01d2843f7acedc9d
Mahliyo <gasandigital13@gmail.com>
2026-04-26 18:42:41 +0500

    fetch javascript render img

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ fetch("https://699d9b4283e60a406a46e1ba.mockapi.io/Teachers")
 })
  function renderData(){
     const card = data.map(el => `{
-        
+        <div class="card">
+            <img src="${el.avatar}" alt="${el.name}">
+             <p class="text">${el.name}</p>
+            <a class="data" href="#">${new Date(el.createdAt).toLocaleDateString()}</a>
+    </div>
         }`)
  }
